2b2c74381c588cbd493d109a064194f89b3982dc
AAwesim <asimabazi@hotmail.no>
2017-12-03 16:59:24 +0100

    funker med piltaster nå

--- main.js
+++ main.js
@@ -4,30 +4,43 @@ function drop() {
 }
 
 var slideIndex = 1;
-            showSlides(slideIndex);
+showSlides(slideIndex);
 
-            function nesteSlide(n) {
-                showSlides(slideIndex += n);
+function nesteSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function denneSliden(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("bilder");
+    var kuler = document.getElementsByClassName("kule");
+    if (n > slides.length) {slideIndex = 1}    
+        else if (n < 1) {slideIndex = slides.length}
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";  
             }
 
-            function denneSliden(n) {
-                showSlides(slideIndex = n);
+            for (i = 0; i < kuler.length; i++) {
+                kuler[i].className = kuler[i].className.replace(" active", "");
             }
 
-            function showSlides(n) {
-                var i;
-                var slides = document.getElementsByClassName("bilder");
-                var kuler = document.getElementsByClassName("kule");
-                if (n > slides.length) {slideIndex = 1}    
-                else if (n < 1) {slideIndex = slides.length}
-                for (i = 0; i < slides.length; i++) {
-                    slides[i].style.display = "none";  
-                }
-  
-                for (i = 0; i < kuler.length; i++) {
-                    kuler[i].className = kuler[i].className.replace(" active", "");
-                }
+            slides[slideIndex-1].style.display = "block";  
+            kuler[slideIndex-1].className += " active";
+        }
 
-                slides[slideIndex-1].style.display = "block";  
-                kuler[slideIndex-1].className += " active";
-            }
+
+
+
+        document.onkeydown = function(e) {
+            e = e || window.event;
+            if (e.keyCode == '37') {
+            showSlides(++slideIndex) //left <- show Prev image
+        } else if (e.keyCode == '39') {
+            // right -> show next image
+            showSlides(--slideIndex)
+        }
+    }
