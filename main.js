b266e40a3cde22f5669cac4dc84a0c86ad063a13
AAwesim <asimabazi@hotmail.no>
2017-12-03 13:17:54 +0100

    tilpasser slideshow

--- main.js
+++ main.js
@@ -3,3 +3,31 @@ function drop() {
     document.getElementById("dropdown").classList.toggle("show");
 }
 
+var slideIndex = 1;
+            showSlides(slideIndex);
+
+            function nesteSlide(n) {
+                showSlides(slideIndex += n);
+            }
+
+            function denneSliden(n) {
+                showSlides(slideIndex = n);
+            }
+
+            function showSlides(n) {
+                var i;
+                var slides = document.getElementsByClassName("bilder");
+                var kuler = document.getElementsByClassName("kule");
+                if (n > slides.length) {slideIndex = 1}    
+                if (n < 1) {slideIndex = slides.length}
+                for (i = 0; i < slides.length; i++) {
+                    slides[i].style.display = "none";  
+                }
+  
+                for (i = 0; i < kuler.length; i++) {
+                    kuler[i].className = kuler[i].className.replace(" active", "");
+                }
+
+                slides[slideIndex-1].style.display = "block";  
+                kuler[slideIndex-1].className += " active";
+            }
