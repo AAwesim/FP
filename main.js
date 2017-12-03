4a239af2522bdd4a3bfd8af864728bbf568b8211
AAwesim <asimabazi@hotmail.no>
2017-12-03 13:23:17 +0100

    visuelle endringer

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ var slideIndex = 1;
                 var slides = document.getElementsByClassName("bilder");
                 var kuler = document.getElementsByClassName("kule");
                 if (n > slides.length) {slideIndex = 1}    
-                if (n < 1) {slideIndex = slides.length}
+                else if (n < 1) {slideIndex = slides.length}
                 for (i = 0; i < slides.length; i++) {
                     slides[i].style.display = "none";  
                 }
